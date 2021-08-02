3508f256ad83c4004d986b65eaf730572aae783f
Karen Olson <karmarie@gmail.com>
2021-08-02 15:50:44 -0500

    commit tree 32606e25251bd28c01a098c2220be85e50b182f9

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 // Write your code here!
-/*
-const element = document.createElement('div');
+
+const element = document.createElement("div");
 
 document.body.appendChild(element);
 
-const ul = document.createElement('ul');
+const ul = document.createElement("ul");
 
 for (let i = 0; i < 3; i++) {
-    const li = document.createElement('li');
-    li.innerHTML = (i+1).toString();
-    ul.appendChild(li);
+  const li = document.createElement("li");
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
 }
 
 element.appendChild(ul);
 
 element.style.backgroundColor = "#27647B";
-element.style.fontSize = '24px';
-element.style.marginLeft = '30px';
-element.style.lineHeight = 2; 
-*/
+element.style.fontSize = "24px";
+element.style.marginLeft = "30px";
+element.style.lineHeight = 2;
 
 main.remove();
 
-const newHeader = document.createElement('h1');
+const newHeader = document.createElement("h1");
 
-newHeader.id = 'victory';
-newHeader.innerHTML = 'Karen is the champion';
+newHeader.id = "victory";
+newHeader.innerHTML = "Karen is the champion";
